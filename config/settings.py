57b542f16672e6b24c2d740becc83092a49cc807
Nikhil-ode <nikhilrajput8070@gmail.com>
2026-07-07 14:00:59 +0530

    Fix 403: prioritize JWTAuthentication over TokenAuthentication

--- config/settings.py
+++ config/settings.py
@@ -112,7 +112,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # REST + JWT
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.TokenAuthentication',
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
     'DEFAULT_PERMISSION_CLASSES': (
